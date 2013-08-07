4a20b24f235dc530b1c4ea5fb5483f5ba8fa45ac
Darin Reid <darinreid@gmail.com>
2013-08-07 09:40:03 -0700

    Updated Grunt config

--- Gruntfile.js
+++ Gruntfile.js
@@ -107,13 +107,6 @@ module.exports = function(grunt) {
 			}
 		},
 		rsync: {
-			dev: {
-				src: './',
-				dest: rsyncConfig.dev.dest,
-				host: rsyncConfig.dev.host,
-				recursive: true,
-				syncDest: true
-			},
 			staging: {
 				src: './',
 				dest: rsyncConfig.staging.dest,
@@ -142,11 +135,6 @@ module.exports = function(grunt) {
 	]);
 
 	grunt.registerTask('deploy', [
-		'default',
-		'rsync:dev'
-	]);
-
-	grunt.registerTask('stage', [
 		'default',
 		'rsync:staging'
 	]);
