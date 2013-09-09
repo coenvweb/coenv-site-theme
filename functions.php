ddaab628e82c3fc3d70c4801848cadc7105b7d5c
Darin Reid <darinreid@gmail.com>
2013-09-09 10:17:48 -0700

    Updating print styles

--- functions.php
+++ functions.php
@@ -1094,17 +1094,17 @@ if ( !function_exists( 'coenv_archive_title' ) ) {
 	function coenv_archive_title()
 	{
 		if ( is_category() ) {
-			printf( __( 'Category Archives: %s', 'moskowitz' ), '<span>' . single_cat_title( '', false ) . '</span>' );
+			printf( __( 'Category Archives: %s', 'coenv' ), '<span>' . single_cat_title( '', false ) . '</span>' );
 
 		} elseif ( is_tag() ) {
-			printf( __( 'Tag Archives: %s', 'moskowitz' ), '<span>' . single_tag_title( '', false ) . '</span>' );
+			printf( __( 'Tag Archives: %s', 'coenv' ), '<span>' . single_tag_title( '', false ) . '</span>' );
 
 		} elseif ( is_author() ) {
 			/* Queue the first post, that way we know
 			 * what author we're dealing with (if that is the case).
 			*/
 			the_post();
-			printf( __( 'Author Archives: %s', 'moskowitz' ), '<span class="vcard"><a class="url fn n" href="' . get_author_posts_url( get_the_author_meta( "ID" ) ) . '" title="' . esc_attr( get_the_author() ) . '" rel="me">' . get_the_author() . '</a></span>' );
+			printf( __( 'Author Archives: %s', 'coenv' ), '<span class="vcard"><a class="url fn n" href="' . get_author_posts_url( get_the_author_meta( "ID" ) ) . '" title="' . esc_attr( get_the_author() ) . '" rel="me">' . get_the_author() . '</a></span>' );
 			/* Since we called the_post() above, we need to
 			 * rewind the loop back to the beginning that way
 			 * we can run the loop properly, in full.
@@ -1112,22 +1112,36 @@ if ( !function_exists( 'coenv_archive_title' ) ) {
 			rewind_posts();
 
 		} elseif ( is_day() ) {
-			printf( __( 'Daily Archives: %s', 'moskowitz' ), '<span>' . get_the_date() . '</span>' );
+			printf( __( 'Daily Archives: %s', 'coenv' ), '<span>' . get_the_date() . '</span>' );
 
 		} elseif ( is_month() ) {
-			printf( __( 'Monthly Archives: %s', 'moskowitz' ), '<span>' . get_the_date( 'F Y' ) . '</span>' );
+			printf( __( 'Monthly Archives: %s', 'coenv' ), '<span>' . get_the_date( 'F Y' ) . '</span>' );
 
 		} elseif ( is_year() ) {
-			printf( __( 'Yearly Archives: %s', 'moskowitz' ), '<span>' . get_the_date( 'Y' ) . '</span>' );
+			printf( __( 'Yearly Archives: %s', 'coenv' ), '<span>' . get_the_date( 'Y' ) . '</span>' );
 
 		} else {
-			_e( 'Archives', 'moskowitz' );
+			_e( 'Archives', 'coenv' );
 
 		}
 	}
 
 }
 
+/**
+ * Print breadcrumbs
+ * For print stylesheet only
+ */
+function coenv_print_breadcrumbs() {
+	global $post;
+
+	print '<pre>';
+	print_r($post->ancestors);
+	print '</pre>';
+
+	$output = get_bloginfo('url');
+	echo $output;
+}
 
 
 
