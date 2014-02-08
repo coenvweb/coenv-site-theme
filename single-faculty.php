68d531aa6c70f395a54550dfdb2bb283655ab009
Darin Reid <darinreid@gmail.com>
2014-02-07 16:38:04 -0800

    Faculty: single member profile header responsive updates

--- single-faculty.php
+++ single-faculty.php
@@ -43,86 +43,90 @@ if ( !empty( $member['stories'] ) ) {
 <section class="Faculty-single" role="main">
 	
 	<div class="container">
+
+		<div class="Faculty-single-container">
 		
-		<nav class="Faculty-header">
+			<nav class="Faculty-header">
 
-			<div class="Faculty-header-inner">
+				<div class="Faculty-header-inner">
+					
+					<div class="Faculty-header-top">
+						<a class="Faculty-header-link" href="<?php bloginfo('url') ?>/faculty/"><i class="icon-faculty-grid-alt-2"></i> Search and filter faculty</a>
+					</div>
+
+					<div class="Faculty-header-bottom">
+						<h1 class="Faculty-header-title">Faculty</h1>
+					</div>
+
+				</div><!-- .Faculty-header-inner -->
+
+			</nav><!-- .Faculty-header -->
+
+			<header class="Faculty-member-header">
 				
-				<div class="Faculty-header-top">
-					<a class="Faculty-header-link" href="<?php bloginfo('url') ?>/faculty/"><i class="icon-faculty-grid-alt-2"></i> Search and filter faculty</a>
-				</div>
+				<div class="Faculty-member-image">
 
-				<div class="Faculty-header-bottom">
-					<h1 class="Faculty-header-title">Faculty</h1>
-				</div>
-
-			</div><!-- .Faculty-header-inner -->
-
-		</nav><!-- .Faculty-header -->
-
-		<header class="Faculty-member-header">
-			
-			<div class="Faculty-member-image">
-
-				<div class="Faculty-member-image-inner">
-						
-					<?php if ( !empty( $member['images']['large'] ) ) : ?>
-
-						<img src="<?php echo $member['images']['medium']['url'] ?>" alt="Portrait of <?php echo $member['full_name'] ?>" />
-
-					<?php endif ?>
-
-				</div><!-- .Faculty-member-image-inner -->
-
-			</div><!-- .Faculty-member-image -->
-
-			<div class="Faculty-member-vcard">
-
-				<div class="Faculty-member-vcard-inner" style="background-color: <?php echo $member['units'][0]['color'] ?>;">
-
-					<div class="Faculty-member-vcard-content">
-
-						<div class="Faculty-member-vcard-content-inner">
+					<div class="Faculty-member-image-inner">
 							
-							<h1 class="Faculty-member-name"><?php echo $member['full_name'] ?></h1>
+						<?php if ( !empty( $member['images']['large'] ) ) : ?>
 
-							<?php if ( !empty( $member['academic_title'] ) ) : ?>
-								<h2 class="Faculty-member-academic-title"><?php echo $member['academic_title'] ?></h2>
-							<?php endif ?>
+							<img src="<?php echo $member['images']['medium']['url'] ?>" alt="Portrait of <?php echo $member['full_name'] ?>" />
 
-							<?php if ( !empty( $member['units'] ) ) : ?>
-								<h3 class="Faculty-member-unit"><a href="<?php echo $member['units'][0]['url'] ?>"><?php echo $member['units'][0]['name'] ?></a></h3>
-							<?php endif ?>
+						<?php endif ?>
 
-							<?php if ( !empty( $member['themes'] ) ) : ?>
+					</div><!-- .Faculty-member-image-inner -->
 
-								<div class="Faculty-member-themes">
+				</div><!-- .Faculty-member-image -->
 
-									Working on: 
+				<div class="Faculty-member-vcard">
 
-									<ul>
+					<div class="Faculty-member-vcard-inner" style="background-color: <?php echo $member['units'][0]['color'] ?>;">
 
-										<?php foreach ( $member['themes'] as $theme ) : ?>
+						<div class="Faculty-member-vcard-content">
 
-											<li><a href="<?php echo $theme['url'] ?>"><?php echo $theme['name'] ?></a></li>
+							<div class="Faculty-member-vcard-content-inner">
+								
+								<h1 class="Faculty-member-name"><?php echo $member['full_name'] ?></h1>
 
-										<?php endforeach ?>
+								<?php if ( !empty( $member['academic_title'] ) ) : ?>
+									<h2 class="Faculty-member-academic-title"><?php echo $member['academic_title'] ?></h2>
+								<?php endif ?>
 
-									</ul>
+								<?php if ( !empty( $member['units'] ) ) : ?>
+									<h3 class="Faculty-member-unit"><a href="<?php echo $member['units'][0]['url'] ?>"><?php echo $member['units'][0]['name'] ?></a></h3>
+								<?php endif ?>
 
-								</div><!-- .Faculty-member-themes -->
+								<?php if ( !empty( $member['themes'] ) ) : ?>
 
-							<?php endif ?>
+									<div class="Faculty-member-themes">
 
-						</div><!-- .Faculty-member-vcard-content-inner -->
-						
-					</div><!-- .Faculty-member-vcard-content -->
+										Working on: 
 
-				</div><!-- .Faculty-member-vcard-inner -->
+										<ul>
 
-			</div><!-- .Faculty-member-vcard -->
+											<?php foreach ( $member['themes'] as $theme ) : ?>
 
-		</header><!-- .Faculty-member-header -->
+												<li><a href="<?php echo $theme['url'] ?>"><?php echo $theme['name'] ?></a></li>
+
+											<?php endforeach ?>
+
+										</ul>
+
+									</div><!-- .Faculty-member-themes -->
+
+								<?php endif ?>
+
+							</div><!-- .Faculty-member-vcard-content-inner -->
+							
+						</div><!-- .Faculty-member-vcard-content -->
+
+					</div><!-- .Faculty-member-vcard-inner -->
+
+				</div><!-- .Faculty-member-vcard -->
+
+			</header><!-- .Faculty-member-header -->
+
+		</div><!-- .Faculty-single-container -->
 
 	</div><!-- .container -->
 
