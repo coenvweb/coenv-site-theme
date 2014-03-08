a045209d5622b455f22f41f41b9ccb61f624d835
Joe Eastham <jeastham@gmail.com>
2014-03-08 15:43:20 -0800

    Removing faculty redirects

--- single-faculty.php
+++ single-faculty.php
@@ -2,10 +2,6 @@
 /**
  * Template to show single faculty member
  */
-// Redirect to /coenv-faculty/ if not logged in
-if ( !is_user_logged_in() ) {
-	wp_redirect( get_bloginfo('url') . '/coenv-faculty/' );
-}
 
 
 get_header();
