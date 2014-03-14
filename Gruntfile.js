06b40f02c0ecfbb23fea12e59ba87c7a0f70714e
Drew Collins <drewcoll@uw.edu>
2014-03-14 11:02:37 -0700

    Building default function for grunt

--- Gruntfile.js
+++ Gruntfile.js
@@ -24,15 +24,58 @@ module.exports = function(grunt) {
 		 * Concat and minify all scripts and plugins
 		 */
 		uglify: {
-			dev: {
+			prod: {
 				options: {
 					sourceMap: '<%= paths.dev %>assets/scripts/maps/main.js.map',
 					sourceMapRoot: '../src/',
 					sourceMappingURL: '../maps/main.js.map',
 					sourceMapPrefix: '3'
+				},
+				files: {
+					// The main script file
+					'<%= paths.dev %>assets/scripts/build/main.min.js': [
+						'<%= paths.dev %>bower_components/jquery/jquery-migrate.min.js',
+						'<%= paths.dev %>bower_components/jquery-fast-click/jQuery.fastClick.js',
+						'<%= paths.dev %>bower_components/jquery-throttle-debounce/jquery.ba-throttle-debounce.js',
+						'<%= paths.dev %>bower_components/chosen/chosen/chosen.jquery.js',
+						'<%= paths.dev %>bower_components/fitvids/jquery.fitvids.js',
+						'<%= paths.dev %>bower_components/jquery-placeholder/jquery.placeholder.js',
+						'<%= paths.dev %>bower_components/jquery-hoverIntent/jquery.hoverIntent.js',
+						'<%= paths.dev %>bower_components/picturefill/picturefill.js',
+						'<%= paths.dev %>bower_components/enquire/dist/enquire.js',
+						'<%= paths.dev %>assets/scripts/src/plugins/jquery.royalslider.js',
+						'<%= paths.dev %>assets/scripts/src/plugins/royalslider-modules/jquery.rs.auto-height.js',
+						'<%= paths.dev %>assets/scripts/src/plugins/royalslider-modules/jquery.rs.autoplay.js',
+						'<%= paths.dev %>assets/scripts/src/menu.js',
+						'<%= paths.dev %>assets/scripts/src/features.js',
+						'<%= paths.dev %>assets/scripts/src/blog.js',
+						'<%= paths.dev %>assets/scripts/src/main.js'
+					],
+					// Faculty specific scripts–we'll probably only load this for
+					// faculty directory pages.
+					'<%= paths.dev %>assets/scripts/build/faculty.min.js': [
+						'<%= paths.dev %>bower_components/get-style-property/get-style-property.js',
+						'<%= paths.dev %>bower_components/get-size/get-size.js',
+						'<%= paths.dev %>bower_components/jquery.scrollTo/jquery.scrollTo.js',
+						//'<%= paths.dev %>bower_components/isotope/jquery.isotope.js',
+						'<%= paths.dev %>assets/scripts/src/plugins/isotope2.js',
+						//'<%= paths.dev %>assets/scripts/src/plugins/procession/jquery.procession.js',
+						'<%= paths.dev %>assets/scripts/src/faculty-list.js',
+						'<%= paths.dev %>assets/scripts/src/faculty-toolbox.js'
+					],
+
+					// jQuery fallback. Load this if CDN version is not available (user is offline)
+					'<%= paths.dev %>assets/scripts/build/jquery-fallback.min.js': [
+						'<%= paths.dev %>assets/scripts/src/jquery-fallback.js'
+					],
+
+					// Admin specific-scripts
+					'<%= paths.dev %>assets/scripts/build/admin.min.js': [
+						'<%= paths.dev %>assets/scripts/src/customNavSubheadCheckboxes.js'
+					]
 				}
 			},
-			dist: {
+			dev: {
 				options: {
 					mangle: false,
 					compress: false,
@@ -120,7 +163,7 @@ module.exports = function(grunt) {
 				},
 				files: {
 					'<%= paths.dev %>assets/styles/build/screen.css' : [
-						'<%= paths.dev %>.tmp/assets/styles/build/screen.css'
+						'<%= paths.dev %>assets/styles/build/screen.css'
 					]
 				}
 			}
@@ -153,15 +196,14 @@ module.exports = function(grunt) {
 				tasks: [ 'sass', 'autoprefixer' ]
 			},
 			css: {
-				files: ['<%= paths.dev %>.tmp/assets/styles/build/**/*.css'],
-				//tasks: [ 'cssmin' ],
+				files: ['<%= paths.dev %>assets/styles/build/**/*.css'],
 				options: {
 					livereload: true
 				}
 			},
 			scripts: {
 				files: ['<%= paths.dev %>assets/scripts/src/**/*.js'],
-				tasks: [ 'jshint', 'uglify' ],
+				tasks: [ 'jshint', 'uglify:dev' ],
 				options: {
 					livereload: true
 				}
@@ -185,20 +227,22 @@ module.exports = function(grunt) {
 	grunt.loadNpmTasks('grunt-contrib-watch');
 
 	grunt.registerTask('dev', [
-		'default',
+		'jshint',
+		'uglify:dev',
+		'sass',
+		'autoprefixer',
 		'watch'
 	]);
 
 	grunt.registerTask('default', [
-		'jshint',
-		'uglify',
-		'sass',
-		'autoprefixer'
-		//'cssmin'
+		'dev'
 	]);
 		
 	grunt.registerTask('prod', [
-		'default',
+		'jshint',
+		'uglify:prod',
+		'sass',
+		'autoprefixer',
 		'cssmin'
 	]);
 
