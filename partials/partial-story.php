43be1d0aeb109164c87ec69ea9d768e6d8b1e468
Drew Collins <drewcoll@uw.edu>
2014-06-25 11:25:59 -0700

    Revert "Story series labels on homepage news stories"
    This reverts commit 1007aa50f7941e1e07bbea6694f5dfe8e2ddddcc.

--- partials/partial-story.php
+++ partials/partial-story.php
@@ -8,20 +8,10 @@
 			</a>
 		<?php endif ?>
 
-		<?php if ( has_category('publications') ): ?> 
-				<div class="story-series">
-					<a href="<?php the_permalink() ?>" name="<?php the_title() ?>">This Week's Publications</a>
-				</div>
-		<?php endif ?>
-		<?php if ( has_category('letter-from-the-dean') ): ?> 
-			<div class="story-series">
-				<a href="<?php the_permalink() ?>" name="<?php the_title() ?>">Letter from the Dean</a>
-			</div>
-		<?php endif ?>
 		<div class="content">
 			<h1><a href="<?php the_permalink() ?>"><?php the_title() ?></a></h1>
 			<?php the_excerpt() ?>
-			<a href="<?php the_permalink() ?>" class="button">Read more »</a>
+            <a href="<?php the_permalink() ?>" class="button">Read more »</a>
 		</div>
 
 	</div><!-- .inner -->
