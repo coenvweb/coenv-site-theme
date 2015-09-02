0cdfecd67cd6ac7f6c2cb88d0baf08f49bef4804
git <coenvweb@uw.edu>
2015-09-02 16:47:12 -0700

    remove printed array from attachment page

--- partials/partial-attachment.php
+++ partials/partial-attachment.php
@@ -1,9 +1,3 @@
-<?php $metadata = wp_get_attachment_metadata() ?>
-
-<?php print '<pre>';
-print_r($metadata);
-print '</pre>'; ?>
-
 <div class="entry-content">
 	<?php echo wp_get_attachment_image( $post->ID, $attachment_size ) ?>
 </div>
