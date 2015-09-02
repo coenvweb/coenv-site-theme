c6ba560e8ea948ae6c7e32d539566d0baadb363d
git <coenvweb@uw.edu>
2015-09-02 16:47:34 -0700

    quick and dirty full-width template

---
+++ templates/full-width.php
@@ -0,0 +1,61 @@
+<?php
+/*
+Template Name: Full width
+*/
+get_header();
+
+$ancestor_id = coenv_get_ancestor();
+
+$ancestor = array(
+	'id' => $ancestor_id,
+	'permalink' => get_permalink( $ancestor_id ),
+	'title' => get_the_title( $ancestor_id )
+);
+?>
+
+	<section id="page" role="main" class="template-page">
+
+		<div class="container">
+
+			<?php if ( in_array( $post->post_type, array('page') ) ) : ?>
+
+				<nav id="secondary-nav" class="side-col">
+
+			<ul id="menu-secondary" class="menu">
+	              <?php wp_list_pages( array(
+	              		'child_of' => $ancestor['id'],
+	                  'depth' => 3,
+	                  'title_li' => '<a href="' . $ancestor['permalink'] . '">' . $ancestor['title'] . '</a>',
+	                  'link_after' => '<i class="icon-arrow-right"></i>',
+	                  'walker' => new CoEnv_Secondary_Menu_Walker,
+	                  'sort_column' => 'menu_order'
+	              ) ) ?>
+	          </ul>
+
+				</nav><!-- #secondary-nav.side-col -->
+
+			<?php endif ?>
+
+			<main id="main-col" class="main-col" style="width: 100%;">
+
+				<?php if ( have_posts() ) : ?>
+
+					<?php while ( have_posts() ) : the_post() ?>
+
+						<?php get_template_part( 'partials/partial', 'article' ) ?>
+
+					<?php endwhile ?>
+
+				<?php endif ?>
+
+			</main><!-- .main-col -->
+
+			<div class="side-col">
+				<?php get_sidebar() ?>
+			</div><!-- .side-col -->
+
+		</div><!-- .container -->
+
+	</section><!-- #page -->
+
+<?php get_footer() ?>
