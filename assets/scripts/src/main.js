7eef93a27d7729c31cae6f8e5c98dbe9a21c7bfe
drewcoll <drewcoll@gmail.com>
2015-11-16 14:11:28 -0800

    update for youtube lightbox

--- assets/scripts/src/main.js
+++ assets/scripts/src/main.js
@@ -22,7 +22,7 @@ jQuery(function ($) {
 		$('.share').coenvshare();
 		
 		// lightbox
-		$('a').nivoLightbox();
+		$('a:not([href*=youtube]):not([href*=youtu.be]):not([href*=vimeo])').nivoLightbox();
         
         $('figure a img').each(function () {
             var $this = $(this);
