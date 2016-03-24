5d5f6002e33c1e8affe7294acf9682c9f9dd7ab3
drewcoll <drewcoll@gmail.com>
2016-03-24 14:28:56 -0700

    accesibility test

--- searchform.php
+++ searchform.php
@@ -1,6 +1,7 @@
 <form role="search" method="get" class="search-form Form--inline" action="<?php echo home_url( '/' ); ?>">
   <div class="field-wrap">
-    <input type="text" value="<?php echo get_search_query() ?>" name="s" id="s" placeholder="Search this site" />
+	<label for="s">Search Field</label>
+    <input type="text" value="<?php echo get_search_query() ?>" name="s" placeholder="Search this site" />
     <button type="submit"><i class="icon-search"></i><span>Search</span></button>
   </div>
 </form>
