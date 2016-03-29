b8c228ffbf5a81250f87b11ada5b1254c5bc0c14
drewcoll <drewcoll@gmail.com>
2016-03-29 15:26:47 -0700

    search field accessibility

--- searchform.php
+++ searchform.php
@@ -1,7 +1,6 @@
 <form role="search" method="get" class="search-form Form--inline" action="<?php echo home_url( '/' ); ?>">
   <div class="field-wrap">
-	<label for="s2">Search Field</label>
-    <input type="text" value="<?php echo get_search_query() ?>" name="s2" id="s2" placeholder="Search this site" />
+	<input type="text" value="<?php echo get_search_query() ?>" name="s" placeholder="Search this site" aria-label="Search" />
     <button type="submit"><i class="icon-search"></i><span>Search</span></button>
   </div>
 </form>
