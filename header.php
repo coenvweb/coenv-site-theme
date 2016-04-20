5cec8d7f7bf2274464e44caca3a1932e1e7346d1
drewcoll <drewcoll@gmail.com>
2016-04-20 14:45:46 -0700

    alert min

--- header.php
+++ header.php
@@ -57,7 +57,7 @@
         <meta name="theme-color" content="#613ba9">
 
         <script src="<?php echo get_template_directory_uri() ?>/assets/scripts/src/plugins/modernizr.custom.92408.js"></script>
-		<script src="//www.washington.edu/static/alert.js" type="text/javascript"></script>
+		<script src="//www.washington.edu/static/alert.min.js" type="text/javascript"></script>
     <script>
         (function(d) {
                 var config = {
