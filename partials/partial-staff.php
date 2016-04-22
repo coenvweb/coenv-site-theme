0fbdc712bcfca4d649b5ae246bcf0b08517c0508
drewcoll <drewcoll@gmail.com>
2016-04-22 12:21:56 -0700

    finish staff content type

--- partials/partial-staff.php
+++ partials/partial-staff.php
@@ -17,9 +17,15 @@ $phone_number = get_field('phone_number');
             <h3><?php the_title(); ?></h3>
             <h4><?php the_field('job_title'); ?></h4>
         </div>
-        <ul class="Faculty-member-contact-list">
-            <li><a href="mailto:<?php echo $email; ?>"><i class="icon-contact-link-email"></i><?php echo $email; ?></a></li>
-            <li><a href="tel:<?php echo $phone_number; ?>"><i class="icon-contact-link-phone"></i><?php echo $phone_number; ?></a></li>
-        </ul>
+        <?php if (!empty($email) && !empty($phone_number)) : ?>
+            <ul class="Faculty-member-contact-list">
+                <?php if (!empty($email)) : ?>
+                    <li><a href="mailto:<?php echo $email; ?>"><i class="icon-contact-link-email"></i><?php echo $email; ?></a></li>
+                <?php endif; ?>
+                <?php if (!empty($phone_number)) : ?>
+                    <li><a href="tel:<?php echo $phone_number; ?>"><i class="icon-contact-link-phone"></i><?php echo $phone_number; ?></a></li>
+                <?php endif; ?>
+            </ul>
+        <?php endif; ?>
     </div>
 </div>
