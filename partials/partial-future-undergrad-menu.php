0c87e3471e5d5c350db4c422ae22e5060c4796f1
drewcoll <drewcoll@gmail.com>
2017-06-20 16:10:03 -0700

    smaller majors section

--- partials/partial-future-undergrad-menu.php
+++ partials/partial-future-undergrad-menu.php
@@ -6,7 +6,7 @@
         <li class="arrow-button arrow-1 <?php if ((get_the_title() == 'Prepare') || (get_the_title() == 'Opportunities for Early Explorers')) { echo ' active';};?>"><a href="/students/future-students/future-undergrads/prepare/">Prepare</a></li>
         <li class="arrow-button arrow-2 <?php if (get_the_title() == 'Connect') { echo ' active';};?>"><a href="/students/future-students/future-undergrads/connect/">Connect</a></li>
         <li class="arrow-button arrow-2 <?php if (get_the_title() == 'Visit' || (get_the_title() == 'Future Student Visit Day')) { echo ' active';};?>"><a href="/students/future-students/future-undergrads/visit/">Visit</a></li>
-        <li class="arrow-button arrow-3 <?php if (get_the_title() == 'Apply') { echo ' active';};?>"><a href="/students/future-students/future-undergrads/apply/">Apply</a></li>
+        <li class="arrow-button arrow-3 <?php if (get_the_title() == 'Join us') { echo ' active';};?>"><a href="/students/future-students/future-undergrads/apply/">Join us</a></li>
     </ul>
 
 </nav><!-- #secondary-nav.side-col -->
