c5040c5172601e92c35aecb4afc42ecdd3195d1b
drewcoll <drewcoll@gmail.com>
2017-10-02 15:10:11 -0700

    fix connect link

--- partials/partial-future-grad-menu.php
+++ partials/partial-future-grad-menu.php
@@ -4,7 +4,7 @@
 
     <ul id="menu-secondary" class="arrow-menu">
         <li class="arrow-button arrow-1 <?php if ((get_the_title() == 'Prepare') || (get_the_title() == 'Future Student Opportunities')) { echo ' active';};?>"><a href="/students/future-students/future-graduate-students/prepare/">Prepare</a></li>
-        <li class="arrow-button arrow-2 <?php if (get_the_title() == 'Connect') { echo ' active';};?>"><a href="/students/future-graduate-students/connect/">Connect</a></li>
+        <li class="arrow-button arrow-2 <?php if (get_the_title() == 'Connect') { echo ' active';};?>"><a href="/students/future-students/future-graduate-students/connect/">Connect</a></li>
         <li class="arrow-button arrow-2 <?php if (get_the_title() == 'Visit' || (get_the_title() == 'Future Student Visit Day')) { echo ' active';};?>"><a href="/students/future-students/future-graduate-students/visit/">Visit</a></li>
         <li class="arrow-button arrow-3 <?php if (get_the_title() == 'Join Us') { echo ' active';};?>"><a href="/students/future-students/future-graduate-students/join-us/">Join Us</a></li>
     </ul>
