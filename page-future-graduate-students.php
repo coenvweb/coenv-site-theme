ebc43cc7d5fce79a08dc25ad6abba903d4805e80
drewcoll <drewcoll@gmail.com>
2017-11-03 14:20:59 -0700

    break it

--- page-future-graduate-students.php
+++ page-future-graduate-students.php
@@ -31,7 +31,7 @@ $ancestor = array(
                 </section>
             </article>
             <div class="little-blurb">
-                <p>As a graduate student at the College of the Environment, you’ll put science into action to discover the unknown</p>
+                <p>As a graduate student at the College of the Environment, <br />you’ll put science into action to discover the unknown</p>
             </div>
             
         </div>
