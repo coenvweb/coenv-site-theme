a4f31d918799360a3dbc976a4a5d58f897bdc982
Cole Bessee <cbessee@uw.edu>
2017-11-14 14:34:59 -0800

    quick commit of gc gmaps shortcode

--- inc/featured-story-fun.php
+++ inc/featured-story-fun.php
@@ -78,16 +78,18 @@ function big_element_func( $atts ){
     $a = shortcode_atts( array(
         'id' => '1',
     ), $atts );
-    $element_id = ($atts['id'] - 1);
+    $element_id = ($atts['id'] - 1); //start element index at 1
     $rows = get_field('element');
     $active_row = $rows[$element_id];
+
     $element_type = $active_row['element_type'];
     $title = $active_row['element_title'];
     $sub_title = $active_row['element_subtitle'];
     $text = $active_row['text_area'];
     $links = $active_row['links'];
     $photos = $active_row['photos'];
-    if ($element_type !== 'big_gallery') {
+
+    if ($element_type !== 'big_gallery' && $links) {
         $primary_link = $links[0]['link']['url'];
     } else {
         $primary_link = null;
@@ -109,29 +111,66 @@ function big_element_func( $atts ){
         // no rows found
     };
 
+
     $output = '</section><div class="big-element ' . $atts['align'] . ' element-' . $element_type . '"';
     
-        if ($element_type == 'call_to_action'){
-            $output .= ' style="background: linear-gradient( rgba(0, 0, 0, 0.5), rgba(0, 0, 0, 0.5) ), url(' . $photo_url[0] . '); background-size: cover; background-position: center;" ><div class="cta-content">';
-        } else {
-            $output .= '>' . $photo_holder;
+        switch($element_type) {
+            case 'call_to_action':
+                $output .= ' style="background: linear-gradient( rgba(0, 0, 0, 0.5), rgba(0, 0, 0, 0.5) ), url(' . $photo_url[0] . '); background-size: cover; background-position: center;" ><div class="cta-content">';
+                $output .= '<h2 class="title">' . $title . '</h2>';
+                $output .= '<h3 class="subtitle">' . $sub_title . '</h3>';
+                $output .= '<p>' . $text . '</p>';
+                if( !empty($links) ){
+                    foreach ($links as $link) {
+                        $link = $link['link'];
+                        $output .=  '<a class="button" href="' . $link['url'] . '" target="' . $link['target'] . '">' . $link['title'] . '</a>';
+                    }
+                } else {
+                    // no rows found
+                }
+				break;
+            case 'map':
+				$locations = $active_row['locations'];
+				$map_instructions = $active_row['map_instructions'];
+				$marker_icons = $active_row['marker_icons'][0];
+				$active_icon = $marker_icons[0];
+				$inactive_icon = $marker_icons[1];
+                $output .= '><script async defer src="https://maps.googleapis.com/maps/api/js?key=AIzaSyDbky381Xxqtk8LVWJEENojL44RG2oYR14"></script>';
+				$output .= '<div class="acf-map-container">';
+					$output .= '<div class="map-instructions right">'.$map_instructions.'</div>';
+					$output .= '<div class="map-box desktop left"></div>';
+					$output .= '<div class="acf-map" style="height: 400px">';
+						foreach($locations as $location) {
+							$output .= '<div class="marker" data-lat="'.$location['lat'].'" data-lng="'.$location['lng'].'" data-active="'.$active_icon.'" data-inactive="'.$inactive_icon.'">';
+								$output .= '<img class="icon" src="'.$photo_url[0].'" />';
+								$output .= '<h2>'.$location['title'].' </h2>';
+								$output .= '<p class="marker_text">'.$location['text_area'].'</p>';
+							$output .= '</div>';
+						}
+					$output .= '</div>';
+					$output .= '<div class="map-box mobile"></div>';
+				$output .= '</div>';
+				break;
+            case 'profile':
+            case 'gallery':
+            case 'big_gallery':
+            case 'content':
+                $output .= '>' . $photo_holder;
+                $output .= '<h2 class="title">' . $title . '</h2>';
+                $output .= '<h3 class="subtitle">' . $sub_title . '</h3>';
+                $output .= '<p>' . $text . '</p>';
+                if( !empty($links) ){
+                    foreach ($links as $link) {
+                        $link = $link['link'];
+                        $output .=  '<a class="button" href="' . $link['url'] . '" target="' . $link['target'] . '">' . $link['title'] . '</a>';
+                    }
+                } else {
+                    // no rows found
+                }
+				break;
         }
-        $output .= '<h2 class="title">' . $title . '</h2>';
-        $output .= '<h3 class="subtitle">' . $sub_title . '</h3>';
-        $output .= '<p>' . $text . '</p>';
-            
-        if( !empty($links) ){
-            foreach ($links as $link) {
-                $link = $link['link'];
-                $output .=  '<a class="button" href="' . $link['url'] . '" target="' . $link['target'] . '">' . $link['title'] . '</a>';
-            }
-        } else {
-            // no rows found
-        }
-        
-    $output .= '</div><section class="article__content">'; 
-    
-return $output;
+    $output .= '</div><section class="article__content">';
+	return $output;
 };
 add_shortcode( 'big_element', 'big_element_func' );
 
