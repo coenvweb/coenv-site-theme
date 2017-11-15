aa8527efddcb11c4eb901087b5f0b1203fd0fd07
drewcoll <drewcoll@gmail.com>
2017-11-15 13:25:37 -0800

    accent color

--- templates/featured-story.php
+++ templates/featured-story.php
@@ -17,6 +17,12 @@ $ancestor = array(
 $banner = coenv_banner();
 ?>
 
+<script type="stylesheet/css">
+ .element a{
+    color: <?php echo get_field('accent_color'); ?>;
+ }
+</script>
+
 	<section id="blog" role="main" class="template-featured-story">
 
 		<div class="container">
