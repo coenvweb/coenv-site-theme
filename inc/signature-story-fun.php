98b573af4c739838a1c4a94e4a34ed8a7a8a4cb2
drewcoll <drewcoll@gmail.com>
2018-02-02 09:34:54 -0800

    illegal offset error

--- inc/signature-story-fun.php
+++ inc/signature-story-fun.php
@@ -247,7 +247,7 @@ function photo_divider_func( $atts, $content = null ){
         $divider_class = 'default';
     }
     preg_match_all('/wp-image-([\d]+)/', $content, $matches);
-    $output = '</section><div class="photo-divider photo-divider-1' . $divider_class . '" style="background-image:url(' . wp_get_attachment_url($matches[1][0]) . ')"></div>';
+    $output = '</section><div class="photo-divider photo-divider-1 ' . $divider_class . '" style="background-image:url(' . wp_get_attachment_url($matches[1][0]) . ')"></div>';
         if (isset($matches[1][1])) {
         $output .= '<div class="photo-divider photo-divider-2" style="background-image:url(' . wp_get_attachment_url($matches[1][1]) . ')"></div><section class="article__content">';
     } else {
