7b08c6302e926d708cab178bc08d2abd57f739aa
Cole Bessee <cbessee@uw.edu>
2018-04-24 11:28:49 -0700

    fix faculty filter labels at mobile

--- partials/partial-faculty-selector.php
+++ partials/partial-faculty-selector.php
@@ -11,7 +11,7 @@ global $themes, $units, $faculty, $query_args;
 <header class="Faculty-list-header">
 
 	<form action="" class="Faculty-selector">
-		View faculty working on 
+		View faculty working in 
 		<select class="Faculty-selector-theme" name="theme" id="theme-mobile">
 			<option value="theme-all" data-url="<?php bloginfo('url') ?>/faculty/#unit-all">All research themes</option>
 		  	<?php foreach ( $themes as $theme ) : ?>
@@ -20,15 +20,15 @@ global $themes, $units, $faculty, $query_args;
                 
 		  	<?php endforeach ?>
 		</select>
-        <label for="theme-mobile">Research themes</label>
-		in
+        <label for="theme-mobile" style="display:none;">Research themes</label>
+        <div style="margin-bottom: 10px;"></div>
 		<select class="Faculty-selector-unit" name="unit" id="unit-mobile">
 			<option value="unit-all" data-url="<?php bloginfo('url') ?>/faculty/#unit-all">All Schools/Departments</option>
 			<?php foreach ( $units as $unit ) : ?>
 			  <option value="unit-<?php echo $unit['slug'] ?>"<?php if ( $query_args['unit'] == $unit['slug'] ) echo ' selected' ?> data-url="<?php echo $unit['url'] ?>"><?php echo $unit['name'] ?></option>
 			<?php endforeach ?>
 		</select>
-        <label for="unit-mobile">Research themes</label>
+        <label for="unit-mobile" style="display:none;">Research themes</label>
 
 		<input type="submit" value="Submit">
 	</form>
