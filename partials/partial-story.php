c9e6c908a0bce0d7b9ff6c4c21101cf5b14bec76
Joe Eastham <jeastham@gmail.com>
2018-05-03 16:52:39 -0700

    One more partial...change related link target to _self if url is environment.

--- partials/partial-story.php
+++ partials/partial-story.php
@@ -79,7 +79,13 @@
 			<?php echo '<p>' . $trimmed_content . '</p>'; ?>
 
 				<?php if ( get_field('story_link_url') ) { ?>
-					<a href="<?php the_field('story_link_url'); ?>" class="button" target="_blank"><?php the_field('story_source_name'); ?> »</a>
+        <?php if ( strpos(get_field('story_link_url'),"environment.") ) {
+          $env_target = '_self';
+        } else {
+          $env_target = '_blank';
+        }
+        ?>
+        <a href="<?php the_field('story_link_url'); ?>" class="button" target="<?php echo $env_target; ?>"><?php the_field('story_source_name'); ?> »</a>
 				<?php } else { ?>
 					<a href="<?php echo the_permalink(); ?>" class="button">Read more »</a>
 				<?php } ?>
