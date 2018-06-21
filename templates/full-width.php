50f924e7458430e50426895c235ecf0b0450bed6
Joe Eastham <jeastham@gmail.com>
2018-06-21 16:30:23 -0700

    Remove left nav from full-width template

--- templates/full-width.php
+++ templates/full-width.php
@@ -3,38 +3,11 @@
 Template Name: Full width
 */
 get_header();
-
-$ancestor_id = coenv_get_ancestor();
-
-$ancestor = array(
-	'id' => $ancestor_id,
-	'permalink' => get_permalink( $ancestor_id ),
-	'title' => get_the_title( $ancestor_id )
-);
 ?>
 
 	<section id="page" role="main" class="template-page">
 
 		<div class="container">
-
-			<?php if ( in_array( $post->post_type, array('page') ) ) : ?>
-
-				<nav id="secondary-nav" class="side-col">
-
-			<ul id="menu-secondary" class="menu">
-	              <?php wp_list_pages( array(
-	              		'child_of' => $ancestor['id'],
-	                  'depth' => 3,
-	                  'title_li' => '<a href="' . $ancestor['permalink'] . '">' . $ancestor['title'] . '</a>',
-	                  'link_after' => '<i class="icon-arrow-right"></i>',
-	                  'walker' => new CoEnv_Secondary_Menu_Walker,
-	                  'sort_column' => 'menu_order'
-	              ) ) ?>
-	          </ul>
-
-				</nav><!-- #secondary-nav.side-col -->
-
-			<?php endif ?>
 
 			<main id="main-col" class="main-col" style="width: 100%;">
 
