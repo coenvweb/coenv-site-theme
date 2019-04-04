7c74f04e792c53843e37c56313bc494532dcd575
drewcoll <drewcoll@gmail.com>
2019-04-04 16:10:48 -0700

    staff appear in search

--- partials/partial-article-search.php
+++ partials/partial-article-search.php
@@ -6,20 +6,31 @@
 <article id="post-<?php the_ID() ?>" <?php post_class( 'article' ) ?>>
 
     <header class="article__header">
-
-        <?php if ( is_page() || is_single() ) : ?>
-            <h1 class="article__title"><?php the_title() ?></h1>
-        <?php else : ?>
-            <h1 class="article__title"><a href="<?php the_permalink() ?>" rel="bookmark"><?php the_title() ?></a></h1>
-        <?php endif ?>
-
-    </header>
-
-    <section class="article__content row">
+        
         <?php
-            echo get_the_excerpt();
+            $post_type = get_post_type();
+            $post_type_object = get_post_type_object( $post_type );
+            echo '<div class="article__meta"><p class="post-info">' . $post_type_object->labels->singular_name . '</p></div>';
         ?>
 
-    </section>
+        <?php if ( $post_type == 'staff') : ?>
+            <h1 class="article__title"><a href="/about/office-of-the-dean/deans-office-staff/#<?php echo 'bio-t-' . sanitize_title(get_the_title()); ?>" rel="bookmark">Dean's Office Staff</a></h1>
+            </header>
+            <section class="article__content row">
+                <?php echo get_the_title() . ' | ' . get_field( 'job_title', get_the_id() ); ?>
+            </section>
+        <?php elseif ( is_page() || is_single() ) : ?>
+            <h1 class="article__title"><?php the_title() ?></h1>
+            </header>
+            <section class="article__content row">
+                <?php echo get_the_excerpt(); ?>
+            </section>
+        <?php else : ?>
+            <h1 class="article__title"><a href="<?php the_permalink() ?>" rel="bookmark"><?php the_title() ?></a></h1>
+            </header>
+            <section class="article__content row">
+                <?php echo get_the_excerpt(); ?>
+            </section>
+        <?php endif ?>
 
 </article><!-- .article -->
