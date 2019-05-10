3e8f88909a8907ef9d77d0f5187906a4a73ffd5c
drewcoll <drewcoll@gmail.com>
2019-05-10 10:11:06 -0700

    test video

--- header.php
+++ header.php
@@ -148,15 +148,15 @@ $post_link = home_url( add_query_arg( array(), $wp->request ) );
                 <div class="feature-image video-container banner-wrapper">
                 <div class="filter"></div>
                     <?php
-                        echo '<video autoplay loop muted class="fillWidth fullfade show-for-medium-up" id="hero-video" poster="' . get_bloginfo('template_directory') . '/assets/video/ess-video.jpg">';
-                        echo '<source src="' . get_bloginfo('template_directory') . '/assets/video/ess-video.mp4" type="video/mp4" />Your browser does not support the video tag. I suggest you upgrade your browser.';
-                        echo '<source src="' . get_bloginfo('template_directory') . '/assets/video/ess-video.webm" type="video/webm" />Your browser does not support the video tag. I suggest you upgrade your browser.';
-                        echo '<source src="' . get_bloginfo('template_directory') . '/assets/video/ess-video.ogg" type="video/ogg"/>Your browser does not support the video tag. I suggest you upgrade your browser.';
+                        echo '<video autoplay loop muted class="fillWidth fullfade show-for-medium-up" id="hero-video" poster="' . get_bloginfo('template_directory') . '/assets/video/mekong-video-720.jpg">';
+                        echo '<source src="' . get_bloginfo('template_directory') . '/assets/video/mekong-looper-720.mp4" type="video/mp4" />Your browser does not support the video tag. I suggest you upgrade your browser.';
+                        echo '<source src="' . get_bloginfo('template_directory') . '/assets/video/mekong-video-720.webm" type="video/webm" />Your browser does not support the video tag. I suggest you upgrade your browser.';
+                        echo '<source src="' . get_bloginfo('template_directory') . '/assets/video/mekong-video-720.ogg" type="video/ogg"/>Your browser does not support the video tag. I suggest you upgrade your browser.';
                         echo '</video>';
                      ?>
                     <div class="poster visuallyhidden">
                         <?php
-                        echo '<img src="' . get_bloginfo('template_directory') . '/assets/video/ess-video.jpg" alt="">';
+                        echo '<img src="' . get_bloginfo('template_directory') . '/assets/video/mekong-video.jpg" alt="">';
                          ?>
                     </div>
                     <div class="layout-container layout-container--header">
