b899184a94d04af20c266e440ef10f50efda6d2e
drewcoll <drewcoll@gmail.com>
2019-05-17 15:44:41 -0700

    sider indicators

--- assets/scripts/src/slider.js
+++ assets/scripts/src/slider.js
@@ -2,21 +2,21 @@ jQuery(function ($) {
 	'use strict';
 
 	// no slider animations for < ie8
-    if ( !$('body').is('.lt-ie8 .post-template-cambodia-signature-story' )) {
-        if ( $('body').is('.post-template-cambodia-signature-story' )) {
+    if (!$('body').is('.lt-ie8 .post-template-cambodia-signature-story')) {
+        if ($('body').is('.post-template-cambodia-signature-story')) {
             $('.big-element.element-slider_gallery').royalSlider({
                 loop: true,
                 autoScaleSlider: true,
                 imageScaleMode: 'fill',
-                controlNavigation: 'bullets',
+                controlNavigation: 'thumbnails',
                 arrowsNav: true,
                 transitionType: 'fade',
                 controlsInside: false,
                 arrowsNavAutoHide: false,
                 block: {
-                    fadeEffect: true,
+                    fadeEffect: true
                 }
-              });
+            });
             $('.element.element-').royalSlider({
                 loop: true,
                 autoScaleSlider: true,
@@ -31,12 +31,58 @@ jQuery(function ($) {
                 controlsInside: false,
                 arrowsNavAutoHide: false,
                 block: {
-                    fadeEffect: true,
+                    fadeEffect: true
                 }
-              });
+            });
+            
+            function sliderGallery( slider ) {
+                //set up nav links
+                var $container = $('article'),
+                    $rsContainer = $container.find(slider),
+                    $features = $container.find('.rsSlide'),
+                    $nav = $('<div></div>'),
+                    offsetTop,
+                    rsInstance,
+                    navOutput;
+                rsInstance = $rsContainer.data('royalSlider');
+
+                console.log($rsContainer);
+                navOutput = '<ul>';
+
+                $nav.addClass('features-nav');
+
+                for (var i = 0, len = rsInstance.numSlides; i < len; i++ ) {
+                    navOutput += '<li><a href="#"><span>' + ( i + 1 ) + '</span></a></li>';
+                }
+
+                navOutput += '</ul>';
+
+                $nav.append( $(navOutput) );
+                $nav.appendTo( $rsContainer );
+                console.log(rsInstance.currSlideId);
+                $nav.find('a').eq( rsInstance.currSlideId ).addClass( 'active' );
+
+                $nav.find('a').on( 'click', function (e) {
+                    e.preventDefault();
+                    $rsContainer.royalSlider( 'goTo', $(this).parent().index() );
+                    $nav.find('a').removeClass( 'active' );
+                    $(this).addClass( 'active' );
+                } );
+
+                //update nav on slide change
+                rsInstance.ev.on( 'rsAfterSlideChange', function () {
+                    $nav.find('a').removeClass( 'active' );
+                    $nav.find('a').eq( rsInstance.currSlideId ).addClass( 'active' );
+                });
+            };
+            
+            sliderGallery('.element-slider_gallery1');
+            sliderGallery('.element-slider_gallery3');
+            sliderGallery('.element-slider_gallery5');
         }
     }
 });
+    
 
 //$.fn.sliderGallery = function() {
 	//'use strict';
@@ -58,9 +104,6 @@ jQuery(function ($) {
   
 
 	// init royalSlider
-	
-
-	//rsInstance = $rsContainer.data('royalSlider');
 
 	//$features.removeClass('loading');
 
@@ -77,34 +120,5 @@ jQuery(function ($) {
 	//		rsInstance.startAutoPlay();
 		//}
 	//} ) );
-
-	// set up nav links
-	//navOutput = '<ul>';
-
-	//$nav.addClass('features-nav');
-
-	//for ( var i = 0, len = rsInstance.numSlides; i < len; i++ ) {
-	//	navOutput += '<li><a href="#"><span>' + ( i + 1 ) + '</span></a></li>';
-	//}
-
-	//navOutput += '</ul>';
-
-	//$nav.append( $(navOutput) );
-	//$nav.appendTo( $container );
-
-	//$nav.find('a').eq( rsInstance.currSlideId ).addClass( 'active' );
-
-	//$nav.find('a').on( 'click', function (e) {
-	//	e.preventDefault();
-	//	$rsContainer.royalSlider( 'goTo', $(this).parent().index() );
-	//	$nav.find('a').removeClass( 'active' );
-	//	$(this).addClass( 'active' );
-	//} );
-
-	// update nav on slide change
-	//rsInstance.ev.on( 'rsAfterSlideChange', function () {
-	//	$nav.find('a').removeClass( 'active' );
-	//	$nav.find('a').eq( rsInstance.currSlideId ).addClass( 'active' );
-	//} );
 
 //};
