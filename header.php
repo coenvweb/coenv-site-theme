e62e6d796f67530f9a95ecb27f8efcd4a8f7e25a
drewcoll <drewcoll@gmail.com>
2019-05-30 10:31:14 -0700

    zoe edits and new video

--- header.php
+++ header.php
@@ -158,7 +158,7 @@ $post_link = home_url( add_query_arg( array(), $wp->request ) );
                 <div class="filter"></div>
                     <?php
                         echo '<video autoplay loop muted class="fillWidth fullfade show-for-medium-up" id="hero-video" poster="' . get_bloginfo('template_directory') . '/assets/video/mekong-video.jpg">';
-                        echo '<source src="' . get_bloginfo('template_directory') . '/assets/video/mekong-video-720.mp4" type="video/mp4" />Your browser does not support the video tag. I suggest you upgrade your browser.';
+                        echo '<source src="' . get_bloginfo('template_directory') . '/assets/video/mekong-looper-720.mp4" type="video/mp4" />Your browser does not support the video tag. I suggest you upgrade your browser.';
                         echo '<source src="' . get_bloginfo('template_directory') . '/assets/video/mekong-video-720.webm" type="video/webm" />Your browser does not support the video tag. I suggest you upgrade your browser.';
                         echo '<source src="' . get_bloginfo('template_directory') . '/assets/video/mekong-video-720.ogg" type="video/ogg"/>Your browser does not support the video tag. I suggest you upgrade your browser.';
                         echo '</video>';
