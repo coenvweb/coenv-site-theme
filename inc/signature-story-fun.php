c331fd0ce0bbd6afcce613c00f22bf1aa79f9d55
drewcoll <drewcoll@gmail.com>
2019-05-31 11:17:21 -0700

    gallery caption accesibility

--- inc/signature-story-fun.php
+++ inc/signature-story-fun.php
@@ -60,8 +60,8 @@ function element_func( $atts ){
             
             if ($element_type == 'slider_gallery') {
                 $photo_el = wp_get_attachment_image( $photo['ID'], 'original' );
+                //$photo_el .= '<a class="photo rsImg" href="' . $photo_el . '">' . $photo['caption'] . '</a>';
                 $photo_holder .= $photo_el;
-                //$photo_holder .= '<a class="photo rsImg" href="' . $photo_el . '">' . $photo['caption'] . '</a>';
                 $element_extra_class .= ' rsDefault';
             } else {
                 $photo_holder .= '<a class="photo" href="' . $top_link . '" title="' . $photo['caption'] . '" ' . $gallery . '>' . $photo_el . '</a>';
@@ -152,7 +152,7 @@ function big_element_func( $atts ){
                 $photo_el = wp_get_attachment_image( $photo['ID'], 'original' );
                 $photo_alt = $photo['alt'];
                 //$photo_holder .= $photo_el;
-                $photo_holder .= '<a class="photo rsImg" href="' . $photo_url[0] . '" alt="' . $photo_alt . '">' . $caption . '</a>';
+                $photo_holder .= '<div class="slide-and-caption"><img class="photo" src="' . $photo_url[0] . '" alt="' . $photo_alt . '"></img><div class="caption-wrapper"><p class="specialCaption">' . $caption . '</p></div></div>';
                 $element_extra_class .= ' rsDefault';
             } else {
                 $photo_holder .= '<a class="photo" href="' . $top_link . '" title="' . $photo['caption'] . '" ' . $gallery . '>' . $photo_el . '</a>';
