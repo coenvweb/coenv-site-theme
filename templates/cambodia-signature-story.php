35e2c8f44da022d7aea25b7fbefd788dc64d249d
drewcoll <drewcoll@gmail.com>
2019-06-10 10:52:57 -0700

    remove ipad video controls

--- templates/cambodia-signature-story.php
+++ templates/cambodia-signature-story.php
@@ -171,7 +171,7 @@ $post_link = home_url( add_query_arg( array(), $wp->request ) );
                     <div class="feature-image video-container banner-wrapper">
                     <div class="filter"></div>
                         <?php
-                            echo '<video autoplay loop muted class="fillWidth fullfade show-for-medium-up" id="hero-video" poster="https://coenv-media-gene1ufvxiloffjq.stackpathdns.com/2018/05/20190114_CAMBODIA-FISH-C-OF-E_02263-e1559260230602.jpg">';
+                            echo '<video autoplay loop muted class="fillWidth fullfade show-for-medium-up" onplaying="this.controls=false" id="hero-video" poster="https://coenv-media-gene1ufvxiloffjq.stackpathdns.com/2018/05/20190114_CAMBODIA-FISH-C-OF-E_02263-e1559260230602.jpg">';
                             echo '<source src="' . get_bloginfo('template_directory') . '/assets/video/mekong-looper-720.mp4" type="video/mp4" />Your browser does not support the video tag. I suggest you upgrade your browser.';
                             echo '<source src="' . get_bloginfo('template_directory') . '/assets/video/mekong-video-720.webm" type="video/webm" />Your browser does not support the video tag. I suggest you upgrade your browser.';
                             echo '<source src="' . get_bloginfo('template_directory') . '/assets/video/mekong-video-720.ogg" type="video/ogg"/>Your browser does not support the video tag. I suggest you upgrade your browser.';
