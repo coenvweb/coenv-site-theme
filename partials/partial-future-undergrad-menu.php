c2b0f88d19b6ddfd477429b412e8831717398fea
drewcoll <drewcoll@gmail.com>
2019-08-20 15:30:19 -0700

    visit us to apply

--- partials/partial-future-undergrad-menu.php
+++ partials/partial-future-undergrad-menu.php
@@ -6,7 +6,7 @@
         <li class="arrow-button arrow-1 <?php if ((get_the_title() == 'Prepare') || (get_the_title() == 'Future Student Opportunities')) { echo ' active';};?>"><a href="/students/future-students/future-undergrads/prepare/">Prepare</a></li>
         <li class="arrow-button arrow-2 <?php if (get_the_title() == 'Connect') { echo ' active';};?>"><a href="/students/future-students/future-undergrads/connect/">Connect</a></li>
         <li class="arrow-button arrow-2 <?php if (get_the_title() == 'Visit' || (get_the_title() == 'Future Student Visit Day')) { echo ' active';};?>"><a href="/students/future-students/future-undergrads/visit/">Visit</a></li>
-        <li class="arrow-button arrow-3 <?php if (get_the_title() == 'Join Us') { echo ' active';};?>"><a href="/students/future-students/future-undergrads/join-us/">Join Us</a></li>
+        <li class="arrow-button arrow-3 <?php if (get_the_title() == 'Apply') { echo ' active';};?>"><a href="/students/future-students/future-undergrads/apply/">Apply</a></li>
     </ul>
 
 </nav><!-- #secondary-nav.side-col -->
