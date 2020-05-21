10622605ff385353e106b4e01006d4692c3e1eda
drewcoll <drewcoll@gmail.com>
2020-05-21 14:46:29 -0700

    sliders with captions

--- inc/signature-story-fun.php
+++ inc/signature-story-fun.php
@@ -61,15 +61,15 @@ function element_func( $atts ){
             if ($element_type == 'slider_gallery') {
                 $photo_el = wp_get_attachment_image( $photo['ID'], 'original' );
                 //$photo_el .= '<a class="photo rsImg" href="' . $photo_el . '">' . $photo['caption'] . '</a>';
-                $photo_holder .= $photo_el;
-                $element_extra_class .= ' rsDefault';
+                //$photo_holder .= $photo_el;
+                //$element_extra_class .= ' rsDefault';
 
                 // with caption
-                //$photo_holder .= '<div class="slide-and-caption"><img class="photo" src="' . $photo_url[0] . '" alt="' . $photo_alt . '"></img>';
-                //if ($caption) {
-                //    $photo_holder .= '<div class="caption-wrapper"><p class="specialCaption">' . $caption . '</p></div>';
-                //};
-                //$photo_holder .= '</div>';
+                $photo_holder .= '<div class="slide-and-caption"><img class="rsImg" src="' . $photo_url[0] . '" alt="' . $photo_alt . '"></img>';
+                if ($caption) {
+                    $photo_holder .= '<div class="caption-wrapper"><p class="specialCaption">' . $caption . '</p></div>';
+                };
+                $photo_holder .= '</div>';
             } else {
                 $photo_holder .= '<a class="photo" href="' . $top_link . '" title="' . $photo['caption'] . '" ' . $gallery . '>' . $photo_el . '</a>';
             }
