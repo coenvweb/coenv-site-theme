321149484d48c129ed0157ae51df2d48a569693a
drewcoll <drewcoll@gmail.com>
2021-09-15 16:58:14 -0700

    faculty related news

--- partials/partial-related-news.php
+++ partials/partial-related-news.php
@@ -5,37 +5,10 @@
 		$coenv_choice = get_field('related_posts');
 		if ( $coenv_choice == 'related' ) {
 			coenv_related_news( $post->ID );
+		} elseif ( $coenv_choice == 'unit') {
+			coenv_related_news( $coenv_choice );
 		} elseif ( $coenv_choice == 'choose') {
-			$coenv_chosen = get_field('related_posts_post');
-			echo '<div class="related-news"><div class="related-heading">';
-			echo '<h2 class="title">Related News</h2></div>';
-			echo '<div class="related-posts">';
-    		foreach( $coenv_chosen as $post):
-        		setup_postdata($post);
-        		echo '<div class="related-container">';
-        		
-			      echo '<div class="related-thumb">';
-					if ( has_post_thumbnail() ) {
-						echo '<a href="' . get_permalink() . '" title="' . the_title_attribute( 'echo=0' ) . '" rel="bookmark">';
-						echo the_post_thumbnail('medium');
-						echo '</a>';
-					}
-			      echo '</div>';
-        		echo '<div class="related-article-title">';
-						echo '<h3>';
-						echo '<a href="' . get_permalink() . '" title="' . the_title_attribute( 'echo=0' ) . '" rel="bookmark">';
-						echo the_title();
-						echo '</a>';
-						echo '</h3>';
-        		echo '</div>';
-            echo '</div>';
-    		endforeach;
-				echo '<br style="clear:both" />';
-			echo '</div>';
-            echo '</div>';
-            
-   			wp_reset_postdata(); // IMPORTANT - reset the $post object so the rest of the page works correctly
-
+			coenv_related_news( $post->ID );
 		} else {
 
 		}
