360a4610069eacb6498fa2eadb73125f12e45505
drewcoll <drewcoll@gmail.com>
2021-11-18 10:36:59 -0800

    initial commit

--- inc/shortcodes.php
+++ inc/shortcodes.php
@@ -46,4 +46,66 @@ function uwenvironment_trumba_embed( $atts = array() ) {
     return $output;
 }
 add_shortcode('trumba','uwenvironment_trumba_embed');
+
+
+function tile_func($atts, $content = null) {
+  $attributes = shortcode_atts( array(
+      'ids' => '', 
+  ), $atts);
+
+if($attributes['ids']) {
+  $tiles = array($tiles[$attributes['ids']]);
+      $ids = explode(',', $attributes['ids']);
+      $tiles_acf = get_field('tiles');
+      $tiles = array();
+      foreach($ids as $id) {
+          $id = $id - 1;
+          if($tiles_acf[$id]) {
+              $tiles[] = $tiles_acf[$id];
+          }
+      }
+} else {
+    $tiles = get_field('tiles');
+  }
+
+  $output = '<div class="tiles-container">';
+      if($tiles)  {
+          foreach($tiles as $tile) {
+
+            $output .= "<div class='page-tile'>";
+              if($tile['link']) {
+                $output .= '<a class="tile-link" href="'.$tile['link'].'" >';
+              }
+
+                if($tile['image']) {
+                  $image = $tile['image'];
+                  $output .= '<div class="image-wrap"><img src="' . esc_url($image['url']) . '" alt="' . esc_attr($image['alt']) . '" /></div>';
+                }
+
+                $output .= '<div class="content-wrap">';
+
+                  $output .= "<h3 class='title'>".$tile['title']."</h3>";
+
+                  if($tile['subtext']) {
+                    $output .= "<div class='body'>".$tile['subtext']."</div>";
+                  }
+
+                $output .= "</div>";
+
+                if($links) {
+                  $output .= '</a>';
+                }
+
+                $output .= "</div>";
+
+          }
+      }
+      if(count($tiles) >= 2) {
+          $output .= "<div style='clear:both'></div>";
+      }
+  $output .= '</div>';
+  return $output;
+}
+add_shortcode('tiles', 'tile_func');
+
 ?>
