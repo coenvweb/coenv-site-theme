d202587ea8ecc1526e1d282da54de974ab209f6c
joeeastham <jeastham@gmail.com>
2025-04-14 15:18:54 -0700

    podcast coming soon fix - missed a conditional

--- inc/widgets.php
+++ inc/widgets.php
@@ -1649,6 +1649,7 @@ class CoEnv_Widget_Podcast extends WP_Widget {
 				<?xml version="1.0" encoding="UTF-8"?><svg id="microphone" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 107.46 157.33"><defs><style>.cls-1{fill:none;stroke:#fff;stroke-linecap:round;stroke-linejoin:round;stroke-width:11px;}</style></defs><g id="Layer_1-2"><rect class="cls-1" x="29.21" y="5.5" width="49.04" height="95.16" rx="24.52" ry="24.52"/><path class="cls-1" d="M101.96,73.71c0,27.5-21.59,49.79-48.23,49.79S5.5,101.21,5.5,73.71"/><line class="cls-1" x1="53.73" y1="123.5" x2="53.73" y2="151.83"/><line class="cls-1" x1="33.73" y1="151.83" x2="73.73" y2="151.83"/></g></svg>
 				</span>FieldSound</h3>';
 				echo '<p class="small-title">Listen now »</p></a>';
+				echo '<p class="small-title" style="padding-left: 14px; padding-right: 14px; padding-bottom: 14px;">Season 4 coming soon.<br /><a href="https://www.buzzsprout.com/2134643/share">Subscribe now</a></p>';
 				echo '</div>';
 			} else {
 				echo '<div class="podcast-promo-container">';
