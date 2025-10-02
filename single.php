4d1a5be280782e154524f8fe7209fe96e6ecb2bd
drewcoll <drewcoll@gmail.com>
2025-10-02 14:27:05 -0700

    fix columns

--- single.php
+++ single.php
@@ -18,7 +18,7 @@ $ancestor = array(
 $banner = coenv_banner();
 ?>
 
-	<section id="blog" role="main" class="template-blog">
+	<section id="blog" class="template-blog">
 
 		<div class="container">
 
