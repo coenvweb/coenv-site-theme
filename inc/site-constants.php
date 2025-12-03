aaba878b4fabd62b109a97de09a9a3a9e0591f79
drewcoll <drewcoll@gmail.com>
2025-12-02 16:29:04 -0800

    new colors for staging and dev

--- inc/site-constants.php
+++ inc/site-constants.php
@@ -4,13 +4,10 @@
 add_filter('get_user_option_admin_color', 'wpse_313419_conditional_admin_color');
 // 
 function wpse_313419_conditional_admin_color($result) {
-    // Dev: use 'light' color scheme
     if(get_site_url() == 'http://environment.uw.local/wordpress') {
         return 'light';
-    // Staging: use 'blue' color scheme
     } elseif(get_site_url() == 'https://staging.environment.uw.edu/wordpress') {
-        return 'blue';
-    // Production (all other cases): use 'sunrise' color scheme
+        return 'ocean';
     } else {
     }
 }
